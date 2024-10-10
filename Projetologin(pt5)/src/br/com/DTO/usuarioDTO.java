2391f9b07dee244a99a427b43e54115e784c598e
xsMatheussx <136374363+xsMatheussx@users.noreply.github.com>
2024-10-09 23:32:24 -0300

    confirmaçao de sucesso

---
+++ Projetologin(pt5)/src/br/com/DTO/usuarioDTO.java
@@ -0,0 +1,53 @@
+
+package br.com.DTO;
+
+
+public class usuarioDTO {
+    
+    private int idUsuario;
+    private String nomeUsuario, loginUsuario, emailUsuario, senhaUsuario;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getLoginUsuario() {
+        return loginUsuario;
+    }
+
+    public void setLoginUsuario(String loginUsuario) {
+        this.loginUsuario = loginUsuario;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+    
+    
+    
+    
+}
